b5f913c39f1622266e95eb9f740181de50c7123b
Tawfiq Hamid <tawfiq.hamid@gmail.com>
2019-09-24 22:54:29 +0100

    Initial commit

---
+++ api.py
@@ -0,0 +1,50 @@
+from flask import Flask
+from flask import request
+
+import random
+from datetime import datetime
+app = Flask(__name__)
+
+gratitudeReasons = [
+    "Loving parents",
+    "Having a roof over my head",
+    "Good friends",
+    "Always being clothed",
+    "Always being fed",
+    "Good health",
+    "Eyesight",
+    "Being able to walk, talk",
+    "Warmth",
+    "Peaceful political climate",
+];
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/hi')
+def hello_world2():
+    return 'Hi, World!'
+
+
+@app.route('/time')
+def current_time():
+    today = datetime.now()
+    result =  today.strftime("%H:%M:%S on %d %B, %Y: ") + "Hello world"
+    return result;
+
+
+@app.route('/gratitude_simple')
+def gratitudeSimple():
+    random.seed()
+    return random.choice(gratitudeReasons)
+
+@app.route('/gratitude_simple/submit', methods = ['GET'])
+def gratitudeSimpleSubmit():
+    gratitudeMessage = request.args.get('data')
+    gratitudeReasons.append(gratitudeMessage)
+    return gratitudeSimple()
+
+@app.route('/gratitude_simple/all')
+def gratitudeSimpleAll():
+    return "<br />".join(gratitudeReasons)
