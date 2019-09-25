9c6af070f1fe2c391a5c953c5a307c5fa9e15105
Tawfiq Hamid <tawfiq.hamid@gmail.com>
2019-09-25 21:23:55 +0100

    Added database for backend; Added new frontend that calls DB; Moved old frontend to be frontend/simple

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, send_from_directory
+from flask import Flask, request, send_from_directory, render_template, url_for
 
 import random
 from datetime import datetime
@@ -17,10 +17,10 @@ gratitudeReasons = [
     "Peaceful political climate",
 ];
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
 
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+#       Inane miscelania
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 @app.route('/hi')
 def hello_world2():
     return 'Hi, World!'
@@ -32,6 +32,14 @@ def current_time():
     result =  today.strftime("%H:%M:%S on %d %B, %Y: ") + "Hello world"
     return result;
 
+
+
+
+
+
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+#       Gratitude simple
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 @app.route('/gratitude_simple')
 def gratitudeSimple():
@@ -48,15 +56,108 @@ def gratitudeSimpleSubmit():
 def gratitudeSimpleAll():
     return "<br />".join(gratitudeReasons)
 
+
+
+
+
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+#       frontend simple
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
 @app.route('/frontend/<path:path>')
 def send_frontend(path):
     return send_from_directory('frontend', path)
 
-@app.route('/frontend')
+@app.route('/')
 def send_frontend_index():
     return send_from_directory('frontend', "frontend.html")
 
 
 
+
+
+
+
+
+
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+#       DB Setup
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+from flask_sqlalchemy import SQLAlchemy
+import sys
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dataentry.sqlite3'
+db = SQLAlchemy(app)
+
+class Dataentry(db.Model):
+    __tablename__ = "dataentry"
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.Text())
+
+    def __init__ (self, data):
+        self.data = data
+
+db.create_all()
+
+
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+#       Gratitude complex
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+@app.route('/gratitude/submit', methods = ['GET'])
+def post_to_db():
+    message = request.args.get('data')
+
+    newDatum= Dataentry(message)
+    try:
+        db.session.add(newDatum)
+        db.session.commit()
+
+    except Exception as e:
+        print( "\n FAILED entry: {}\n".format( message ) )
+        print(e)
+        sys.stdout.flush()
+
+    return 'Success! Saved: \n' + message;
+
+
+
+@app.route('/gratitude')
+def gratitudeComplex():
+    random.seed()
+    gratitudeReasonObjects = Dataentry.query.all()
+
+    gratitudeReasons = [];
+    for reason in gratitudeReasonObjects:
+        gratitudeReasons.append(reason.data)
+
+    return random.choice(gratitudeReasons)
+
+
+@app.route('/gratitude/all')
+def gratitudeReadAll():
+    gratitudeReasons = Dataentry.query.all()
+
+    allReasons = [];
+
+    for reason in gratitudeReasons:
+        allReasons.append(reason.data)
+
+    return "<br />".join(allReasons)
+
+
+
+
+
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+#       Setup and run main app
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
 if __name__ == "__main__":
+    app.debug = True
     application.run(host='0.0.0.0')
+
+
+# @app.teardown_appcontext
+# def shutdown_session(exception=None):
+#     db.session.remove()
