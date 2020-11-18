58841602bfbe2e3197952cffdb7282b79d8268cc
Tawfiq Hamid <tawfiq.hamid@gmail.com>
2020-11-18 16:56:48 +0000

    Added pipenv

--- startServer.sh
+++ startServer.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
-# . .env
-export FLASK_APP=gratitude
-export  FLASK_ENV=development
-export FLASK_PASSWORD=abc
-export DB_FILE=../dataentry.sqlite3
-echo "FLASK_APP:" $FLASK_APP
 pip install -e . #needed to install gratitude  app too.
 flask run
